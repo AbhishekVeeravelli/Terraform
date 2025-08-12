3b0467e3d3297d1714808b5b2ca61545bd99f490
Abhishek Veeravelli <abhishekveeravelli@Abhisheks-Laptop.local>
2025-08-11 21:51:57 -0600

    Add Terraform cross-reference attributes example

--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
-variable "vpn_ip" {
-  default     = "101.20.30.50/32"
-  description = "This is the VPN server created in AWS"
-  }
-
-variable "app_port" {
-  default     = 8080
-}
-
-variable "ftp_port" {
-  default     = 21
-}
-
-variable "ssh_port" {
-  default     = 22
+variable "ami" {
+  description = "AMI ID for North virginia region"
 }
