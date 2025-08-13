520f0749d010d4649aabc9d9d8cc431b4a973bd5
Abhishek Veeravelli <abhishekveeravelli@Abhisheks-Laptop.local>
2025-08-12 21:35:14 -0600

    Add Terraform cross-reference attributes example

--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-# ami = "ami-0de716d6197524dd9"
-instance_type = "t3.large"
--- variable-precedence.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "myec2" {
-  ami = "ami-0de716d6197524dd9"
-  instance_type = var.instance_type
-}
