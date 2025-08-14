040bae47064a8d5641d86de0b9066fad93c98919
Abhishek Veeravelli <abhishekveeravelli@Abhisheks-Laptop.local>
2025-08-13 21:58:47 -0600

    Add Terraform cross-reference attributes example

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,23 @@ resource "aws_instance" "myec2" {
   ami           = "ami-0de716d6197524dd9" 
   instance_type = "t3.micro"
   count        = 2
+
+  tags = {
+    Name = "MyEC2Instance-${count.index}"
+  }
+}
+
+resource "aws_iam_user" "this" {  
+  name = "my-iam-user-${count.index}"
+  count=3  
+}
+
+variable "users" {
+  type=list
+  default=["Alice","Bob","John"]
+}
+
+resource "aws_iam_user" "that" {  
+  name = var.users[count.index]
+  count=3  
 }
